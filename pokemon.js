44f01123914a48d40b352151b48c9ceebc8752eb
ctabo91 <ctaborda91@gmail.com>
2023-10-19 23:40:31 -0400

    refactored pokemon.js code to use async and await, and created html page to display 3 random pokemon with info.

--- pokemon.js
+++ pokemon.js
@@ -1,19 +1,36 @@
 const BASE_URL = 'https://pokeapi.co/api/v2'
 
 
-let pokemon = [];
-for(let i = 1; i < 4; i++){
-    let randomNum = Math.floor(Math.random() * 1292);
-    pokemon.push(axios.get(`${BASE_URL}/pokemon/${randomNum}/`));
+function randomNum(){
+    return Math.floor(Math.random() * 1000);
 }
 
-    
-Promise.all(pokemon)
-    .then(pokemonArr => (
-        pokemonArr.forEach(p => {
-            axios.get(p.data.species.url)
-                .then(res => {
-                    console.log(`${res.data.name}: ${res.data.flavor_text_entries[0].flavor_text}`);
-                });
-        })
-    ));
+$('button').on('click', async function(){
+    $('#display-area').empty();
+
+    let pokemon = await Promise.all([
+        axios.get(`${BASE_URL}/pokemon/${randomNum()}/`),
+        axios.get(`${BASE_URL}/pokemon/${randomNum()}/`),
+        axios.get(`${BASE_URL}/pokemon/${randomNum()}/`)
+    ]);
+
+    pokemon.forEach(async function(p){
+        let pokemonImg = p.data.sprites.front_default;
+        let name = p.data.name;
+
+        let res = await axios.get(p.data.species.url);
+        let description = res.data.flavor_text_entries[0].flavor_text;
+
+        let html = `
+        <div class="card">
+            <img src="${pokemonImg}" class="card-img-top" alt="${name}">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${description}</p>
+            </div>
+        </div>
+        `;
+        
+        $('#display-area').append(html);
+    })
+});
